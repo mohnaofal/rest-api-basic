1a074a58fd39e8a3cae03e53b0a3aeb7fd7c26de
Moh. Naofal Nurul Huda <moh.naofal@gmail.com>
2020-11-17 08:38:09 +0700

    create user

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -18,9 +19,42 @@ type User struct {
 
 var users []User
 
+func findUserExist(form *User) bool {
+	var exist bool
+	for _, v := range users {
+		if v.ID == form.ID || v.Email == form.Email {
+			exist = true
+			break
+		}
+	}
+
+	return exist
+}
+
 func main() {
 	r := mux.NewRouter()
 
+	r.HandleFunc("/user", Create).Methods("POST")
+
 	fmt.Println("Listening port 9090")
 	http.ListenAndServe(":9090", r)
+}
+
+// Create new user
+func Create(w http.ResponseWriter, r *http.Request) {
+	form := new(User)
+
+	json.NewDecoder(r.Body).Decode(form)
+
+	if findUserExist(form) {
+		w.WriteHeader(400)
+		w.Write([]byte("User already exist"))
+		return
+	}
+
+	users = append(users, *form)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(form)
 }
